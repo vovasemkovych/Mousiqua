c2d3afa4fd51290d1e463ecd2800f90510403d1e
vovasemkovych <37297861+vovasemkovych@users.noreply.github.com>
2022-05-16 18:27:56 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -17,16 +17,16 @@ let smoothScroll = new scrollToSmooth('a', {
     // onScrollUpdate: (data) => { console.log(data); },
     // onScrollEnd: (data) => { console.log(data); },
     fixedHeader: null
-  })
-  smoothScroll.init();
-setTimeout(() => {
-    $(document).ready(function(){
-        $('#spotify').attr('src', 'https://open.spotify.com/embed/album/2ITVvrNiINKRiW7wA3w6w6?utm_source=generator');    
-    });
-      
-  },2000);
-$(document).ready(function() {
-    $('#menu-bar').click();  });
+})
+smoothScroll.init();
+
+$(document).ready(function () {
+    $('#spotify').attr('src', 'https://open.spotify.com/embed/album/2ITVvrNiINKRiW7wA3w6w6?utm_source=generator');
+});
+
+$(document).ready(function () {
+    $('#menu-bar').click();
+});
 
 
 
@@ -37,14 +37,15 @@ $('.smenu__close').click(() => {
     $('.smenu').removeClass('active_flex');
 })
 
-$(document).ready(function() {
-    $('.start_slide2').fadeOut();  });
+$(document).ready(function () {
+    $('.start_slide2').fadeOut();
+});
 
 $('.point1').click(() => {
     $('.start_slide1').fadeIn(500, () => {
         $('.start_slide2').fadeOut(500);
     });
- 
+
 })
 $('.point2').click(() => {
     $('.start_slide2').fadeIn(500, () => {
@@ -52,14 +53,15 @@ $('.point2').click(() => {
     });
 })
 
-$(document).ready(function() {
-    $('.europe').fadeOut();  });
+$(document).ready(function () {
+    $('.europe').fadeOut();
+});
 
 $('.usa').click(() => {
     $('.america').fadeIn(500, () => {
         $('.europe').fadeOut(500);
     });
- 
+
 })
 $('.eur').click(() => {
     $('.europe').fadeIn(500, () => {
@@ -67,15 +69,47 @@ $('.eur').click(() => {
     });
 })
 
-
-document.querySelector(".fas").addEventListener('click', () => {
-    window.open("https://www.youtube.com/watch?v=OS8taasZl8k&ab_channel=RedHotChiliPeppers", '_blank')
-})
+~
+    document.querySelector(".fas").addEventListener('click', () => {
+        window.open("https://www.youtube.com/watch?v=OS8taasZl8k&ab_channel=RedHotChiliPeppers", '_blank')
+    })
 
 $(document).scroll(function () {
     let y = $(this).scrollTop();
     (y > 700) ? $('.header').css('background-color', 'rgba(19, 24, 29, 1)') : $('.header').css('background-color', 'rgba(19, 24, 29, 0.5)');
-  });
+});
+
+ScrollReveal().reveal('.ts', { duration: 1000 }, { delay: 600 }, { origin: 'right' });
+ScrollReveal().reveal('.disk', { duration: 1000 }, { delay: 600 }, { origin: 'left' });
+ScrollReveal().reveal('.time', { duration: 1000 }, { delay: 600 }, { origin: 'right' });
+ScrollReveal().reveal('.band', { duration: 1000 }, { delay: 600 }, { origin: 'left' });
+ScrollReveal().reveal('.tours', { duration: 1000 }, { delay: 600 }, { origin: 'right' });
+ScrollReveal().reveal('.contacts', { duration: 1000 }, { delay: 600 }, { origin: 'left' });
+
+let countDownDate = new Date("Jul 14, 2022 16:00:00").getTime();
+let renew = setInterval(function () {
+    let now = new Date().getTime();
+    let timeleft = countDownDate - now;
+    let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+    document.querySelector(".days").innerHTML = `${days}`
+    document.querySelector(".hours").innerHTML = `${hours}`
+    document.querySelector(".mins").innerHTML = `${minutes}`
+    document.querySelector(".secs").innerHTML = `${seconds}`
+    
+    if (timeleft < 0) {
+        clearInterval(myfunc);
+        document.getElementById("days").innerHTML = ""
+        document.getElementById("hours").innerHTML = "" 
+        document.getElementById("mins").innerHTML = ""
+        document.getElementById("secs").innerHTML = ""
+        document.getElementById("end").innerHTML = "TIME UP!!";
+    }
+}, 1000)
+
 
 // const maxTilt = 30; // Max card tilt (deg).
 
