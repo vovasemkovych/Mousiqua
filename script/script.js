ea8713038274a99bb028024d656f78588adaa274
Volodymyr Semkovych <37297861+vovasemkovych@users.noreply.github.com>
2025-08-13 19:43:09 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,82 +1,95 @@
 'use strict';
+
 function menuOnClick() {
     document.querySelector("#menu-bar").classList.toggle("change");
     document.querySelector("#nav").classList.toggle("change");
     document.querySelector("#menu-bg").classList.toggle("change-bg");
 }
-document.querySelector('#menu-bar').addEventListener('click', menuOnClick())
+document.querySelector('#menu-bar').addEventListener('click', menuOnClick);
 
 let smoothScroll = new scrollToSmooth('a', {
     targetAttribute: 'href',
     duration: 400,
-    durationRelative: false,
-    durationMin: false,
-    durationMax: false,
     easing: 'easeOutCubic',
-    // onScrollStart: (data) => { console.log(data); },
-    // onScrollUpdate: (data) => { console.log(data); },
-    // onScrollEnd: (data) => { console.log(data); },
     fixedHeader: null
-})
+});
 smoothScroll.init();
 
 $(document).ready(function () {
     $('#spotify').attr('src', 'https://open.spotify.com/embed/album/2ITVvrNiINKRiW7wA3w6w6?utm_source=generator');
-});
-
-$(document).ready(function () {
     $('#menu-bar').click();
 });
 
-
-
 $('.search').click(() => {
     $('.smenu').addClass('active_flex');
-})
+});
 $('.smenu__close').click(() => {
     $('.smenu').removeClass('active_flex');
-})
-
-$(document).ready(function () {
-    $('.start_slide2').fadeOut();
 });
 
-$('.point1').click(() => {
-    $('.start_slide1').fadeIn(500, () => {
-        $('.start_slide2').fadeOut(500);
-    });
+// --- SLIDESHOW REFACTOR START ---
 
-})
-$('.point2').click(() => {
-    $('.start_slide2').fadeIn(500, () => {
-        $('.start_slide1').fadeOut(500);
-    });
-})
+document.addEventListener('DOMContentLoaded', function () {
+    // Preload slide backgrounds (if not already in cache)
+    new Image().src = "img/start_back.webp";
+    new Image().src = "img/start_back1.webp";
+
+    const slides = [
+        document.querySelector('.start_slide1'),
+        document.querySelector('.start_slide2')
+    ];
+    const dots = [
+        document.querySelector('.point1'),
+        document.querySelector('.point2')
+    ];
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            if (i === index) {
+                slide.classList.add('active');
+            } else {
+                slide.classList.remove('active');
+            }
+        });
+        dots.forEach((dot, i) => {
+            if (i === index) {
+                dot.classList.add('active');
+            } else {
+                dot.classList.remove('active');
+            }
+        });
+    }
+
+    // Initial state
+    showSlide(0);
+
+    dots[0].addEventListener('click', () => showSlide(0));
+    dots[1].addEventListener('click', () => showSlide(1));
+});
+
+// --- SLIDESHOW REFACTOR END ---
 
 $(document).ready(function () {
     $('.europe').fadeOut();
 });
-
 $('.usa').click(() => {
     $('.america').fadeIn(500, () => {
         $('.europe').fadeOut(500);
     });
-
-})
+});
 $('.eur').click(() => {
     $('.europe').fadeIn(500, () => {
         $('.america').fadeOut(500);
     });
-})
-
+});
 
 document.querySelector(".fas").addEventListener('click', () => {
-    window.open("https://www.youtube.com/watch?v=OS8taasZl8k&ab_channel=RedHotChiliPeppers", '_blank')
-})
-document.querySelector(".tickets").addEventListener('click', () => {
-    window.open("https://www.ticketmaster.com/event/1E005B3E463F5483", '_blank')
-})
+    window.open("https://www.youtube.com/watch?v=OS8taasZl8k&ab_channel=RedHotChiliPeppers", '_blank');
+});
 
+document.querySelector(".tickets").addEventListener('click', () => {
+    window.open("https://www.ticketmaster.com/event/1E005B3E463F5483", '_blank');
+});
 
 $(document).scroll(function () {
     let y = $(this).scrollTop();
@@ -99,62 +112,17 @@ let renew = setInterval(function () {
     let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
-    document.querySelector(".days").innerHTML = `${days}`
-    document.querySelector(".hours").innerHTML = `${hours}`
-    document.querySelector(".mins").innerHTML = `${minutes}`
-    document.querySelector(".secs").innerHTML = `${seconds}`
+    document.querySelector(".days").innerHTML = `${days}`;
+    document.querySelector(".hours").innerHTML = `${hours}`;
+    document.querySelector(".mins").innerHTML = `${minutes}`;
+    document.querySelector(".secs").innerHTML = `${seconds}`;
 
     if (timeleft < 0) {
-        clearInterval(myfunc);
-        document.getElementById("days").innerHTML = ""
-        document.getElementById("hours").innerHTML = ""
-        document.getElementById("mins").innerHTML = ""
-        document.getElementById("secs").innerHTML = ""
+        clearInterval(renew);
+        document.getElementById("days").innerHTML = "";
+        document.getElementById("hours").innerHTML = "";
+        document.getElementById("mins").innerHTML = "";
+        document.getElementById("secs").innerHTML = "";
         document.getElementById("end").innerHTML = "TIME UP!!";
     }
-}, 1000)
-
-
-// const maxTilt = 30; // Max card tilt (deg).
-
-// $(".b-game-card")
-//     .mousemove(function(evt) {
-//         let bounding = mouseOverBoundingElem(evt);
-
-//         let posX = bounding.width / 2 - bounding.x;
-//         let posY = bounding.height / 2 - bounding.y;
-//         let hypotenuseCursor = Math.sqrt(Math.pow(posX, 2) + Math.pow(posY, 2));
-//         let hypotenuseMax = Math.sqrt(Math.pow(bounding.width / 2, 2) + Math.pow(bounding.height / 2, 2));
-//         let ratio = hypotenuseCursor / hypotenuseMax;
-
-//         $(".cover", this).css({
-//             transform: `rotate3d(${posY / hypotenuseCursor}, ${-posX / hypotenuseCursor}, 0, ${ratio * maxTilt}deg)`,
-//             filter: `brightness(${1.6 - bounding.y / bounding.height})` // 0.6 = offset, brightness will be from 0.6 to 1.6
-//         });
-//         $(".gloss", this).css({
-//             transform: `translateX(${posX * ratio * 0.75}px) translateY(${posY * ratio}px)` // 0.75 = offset
-//         });
-//     })
-//     .mouseleave(function() {
-//         let css = {
-//             transform: "",
-//             filter: ""
-//         };
-//         $(".cover, .gloss", this).css(css);
-//     });
-
-// function mouseOverBoundingElem(evt) {
-//     let bounding = evt.target.getBoundingClientRect();
-//     let x = evt.originalEvent.pageX - Math.round(bounding.left);
-//     let y = evt.originalEvent.pageY - Math.round(bounding.top);
-
-//     return {
-//         x: Math.max(0, x),
-//         y: Math.max(0, y),
-//         width: Math.round(bounding.width),
-//         height: Math.round(bounding.height)
-//     };
-// }
-
-
-
+}, 1000);
